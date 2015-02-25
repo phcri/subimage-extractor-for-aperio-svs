482092abf61d1fae9757eb0c885165a1e10ab7c9
DaisukeKinose <DaisukeKinose@users.noreply.github.com>
2015-02-25 12:54:02 -0800

    removed an unnecessary import

--- src/main/java/ca/phcri/Subimage_Extractor.java
+++ src/main/java/ca/phcri/Subimage_Extractor.java
@@ -44,7 +44,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
+//import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
