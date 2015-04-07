73ed6c44e89cbcd22eb1531fe6d5c5952fe2348f
DaisukeKinose <DaisukeKinose@users.noreply.github.com>
2015-04-07 12:23:22 -0700

    Small change

--- src/main/java/ca/phcri/Subimage_Extractor.java
+++ src/main/java/ca/phcri/Subimage_Extractor.java
@@ -30,7 +30,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
-import loci.common.Log4jTools;
 import loci.common.LogbackTools;
 import loci.formats.ChannelSeparator;
 import loci.formats.FormatException;
@@ -47,7 +46,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-//import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
@@ -987,8 +985,9 @@ PlugIn, DialogListener, ActionListener, MouseMotionListener, DocumentListener, F
 						+ "No of subimages vertically",
 						"", 1028, 250);
 				
-				//If a file whose name is String fileName exists in the plugin folder, 
+				//If a file with a name String fileName exists in the plugin folder, 
 				//read it into the list.
+				
 				try {
 					BufferedReader br = 
 							new BufferedReader(
@@ -1004,6 +1003,9 @@ PlugIn, DialogListener, ActionListener, MouseMotionListener, DocumentListener, F
 			            }
 			            historyWindow.append(s);
 					}
+					
+					br.close();
+					
 				} catch (IOException e) {}
 			}
 			
